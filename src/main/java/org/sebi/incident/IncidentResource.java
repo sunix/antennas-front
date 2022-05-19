783ea307b452a724652e48d67f74cda1627af993
Sun Seng David TAN <sutan@redhat.com>
2022-05-19 13:53:20 +0200

    incident service
    Signed-off-by: Sun Seng David TAN <sutan@redhat.com>

--- src/main/java/org/sebi/incident/IncidentResource.java
+++ src/main/java/org/sebi/incident/IncidentResource.java
@@ -2,14 +2,25 @@ package org.sebi.incident;
 
 import java.util.List;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
 
 @Path("rest/incidents")
 public class IncidentResource {
     
+    @Inject
+    @RestClient
+    IncidentService service;
+    
+
     @GET
     public List<Incident> getIncidents(){
-       return Incident.listAll();
+       List<Incident> incidents = service.getAll("secret");
+       System.out.println(incidents);
+       return incidents;
     } 
 }
